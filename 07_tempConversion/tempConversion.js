8b61e1fd2948e280bcdf93cdda58fd2a1b4c09ec
Jefferson Belardo <iamjeffersonbelardo@gmail.com>
2025-01-18 21:46:24 +0800

    Finish exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temperature) {
+    let convertedToCelsius = ((temperature - 32) * 5) / 9;
+    return Math.round(convertedToCelsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temperature) {
+    let convertedToFahrenheit = temperature * (9 / 5) + 32;
+    return Math.round(convertedToFahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+    convertToCelsius,
+    convertToFahrenheit,
 };
