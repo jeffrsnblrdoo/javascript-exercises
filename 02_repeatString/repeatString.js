c7753ec023e8288fb77cb02b2db968be608f28a7
Jefferson Belardo <118502737+jeffrsnblrdoo@users.noreply.github.com>
2025-01-20 18:48:42 +0800

    Finish 1 to 3 exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  let input = "";
+  if (num >= 0) {
+    for (let i = 1; i <= num; i++) {
+      input += string;
+    }
+    return input;
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    //Split the characters of string and returns array
+    const stringArray = string.split("");
+    let reversed = "";
 
+    for (let i = stringArray.length - 1; i >= 0; i--) {
+        reversed += stringArray[i];
+    }
+
+    return reversed;
 };
 
 // Do not edit below this line
